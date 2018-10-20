2150895820301772bb0d49047a1bba197a0461df
Hunter Derrick <Hunderri@uat.edu>
2018-10-20 14:44:01 -0700

    Changed finished date. Submit assignment.

--- Skynet HK-Aerial/Source.cpp
+++ Skynet HK-Aerial/Source.cpp
@@ -2,7 +2,7 @@
 /*
 Name: Hunter Derrick
 Professor: Tony Hinton
-Part 1 Start: 10/16/18  -- Part 1 Finish: 10/16/2018
+Part 1 Start: 10/16/18  -- Part 1 Finish: 10/20/2018
 Project Name: Skynet HK-Aerial
 */
 //Task:
